d983611dbd92f9cf44a9ceb1381408cc26d63a78
Romain Racamier <romain.racamier@externe.e-i.com>
2019-04-12 17:55:49 +0200

    fixed test command

--- tests/check.js
+++ tests/check.js
@@ -5,4 +5,4 @@ const lastScore = 88
 
 assert(score >= lastScore, `Lighthouse current score is ${score} and should be superior or equal to last one : ${lastScore}`)
 
-console.log('\n\nGood ! Lighthouse current score is not worse than last one \\o/\n')
+console.log(`\n\nGood ! Lighthouse current score (${score}) is not worse than last one (${lastScore}) \\o/\n`)
