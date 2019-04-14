714ba9b8867c77ed530cb65a4857e22948a5c68f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-04-14 15:51:50 +0200

    fixed tests & big score bump 88 => 95 \o/

--- tests/check.js
+++ tests/check.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const data = require('./reports/summary.json')
 const score = parseFloat(data[0].score) * 100
-const lastScore = 88
+const lastScore = 95
 
 assert(score >= lastScore, `Lighthouse current score is ${score} and should be superior or equal to last one : ${lastScore}`)
 
