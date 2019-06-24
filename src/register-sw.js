c1db8c74f80e025571399fe045ba1d6b7333e1d0
Romain Racamier <romain.racamier@externe.e-i.com>
2019-06-24 18:40:36 +0200

    trying to fix LH

--- src/register-sw.js
+++ src/register-sw.js
@@ -1,6 +1,5 @@
 // Activate the service worker
-// let hostname 127.0.0.1 have serviceWorker because LightHouse need to check it
-if ('serviceWorker' in navigator && document.location.hostname !== 'localhost') {
+if ('serviceWorker' in navigator && document.location.port !== '8080') {
   window.addEventListener('load', function () {
     // this way Parcel won't look for service-worker.js file
     const swName = 'service-worker.js'
