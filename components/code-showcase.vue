3b41d93745f599fa61a54d4986cb863ec46789f3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-10-07 17:29:32 +0200

    enhance copy feedback

--- components/code-showcase.vue
+++ components/code-showcase.vue
@@ -20,7 +20,7 @@ async function copyCode () {
   // eslint-disable-next-line sonarjs/no-nested-template-literals
   const code = `<${name} ${Object.keys(properties).map(property => `${property}="${properties[property]}"`).join(' ')} />`
   void navigator.clipboard.writeText(code)
-  await sleep(300) // eslint-disable-line @typescript-eslint/no-magic-numbers
+  await sleep(200) // eslint-disable-line @typescript-eslint/no-magic-numbers
   isCopying.value = false
 }
 </script>
@@ -32,8 +32,8 @@ async function copyCode () {
         <slot />
       </Tab>
       <Tab name="second" title="Code">
-        <pre class="relative flex overflow-auto rounded-lg bg-slate-900 px-5 py-4 transition-colors duration-300"
-          :class="{ 'bg-slate-700': isCopying }"><button class="absolute right-5 top-5 text-2xl text-slate-300 hover:text-slate-50" type="button" @click="copyCode"><icon-copy  theme="outline" /></button>&lt;<span class="text-green-300">{{ name }}</span> <div v-for="prop in Object.keys(properties)" :key="prop"><span class="ml-3 text-purple-300">{{ prop }}</span>="<span class="text-blue-300">{{ properties[prop] }}</span>"</div> /&gt;</pre>
+        <pre class="relative flex overflow-auto rounded-lg px-5 py-4 transition-colors duration-300"
+          :class="{ 'bg-slate-700': isCopying, 'bg-slate-900': !isCopying }"><button class="absolute right-5 top-5 text-2xl text-slate-300 transition-transform duration-200 hover:text-slate-50" :class="{ 'scale-75': isCopying, 'hover:scale-110': !isCopying }" type="button" @click="copyCode"><icon-copy  theme="outline" /></button>&lt;<span class="text-green-300">{{ name }}</span> <div v-for="prop in Object.keys(properties)" :key="prop"><span class="ml-3 text-purple-300">{{ prop }}</span>="<span class="text-blue-300">{{ properties[prop] }}</span>"</div> /&gt;</pre>
       </Tab>
     </Tabs>
   </div>
