d3247166ff6a55d46bf06a69b7356f3cba3a2816
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-10-15 14:20:06 +0200

    enhance mobile navbar

--- .vitepress/theme/layouts/base.vue
+++ .vitepress/theme/layouts/base.vue
@@ -5,7 +5,7 @@ const { frontmatter } = useData()
 
 <template>
   <div class="app-layout flex min-h-screen w-full flex-col bg-gradient-to-br from-white to-accent-100 p-3 dark:from-primary-700 dark:to-primary-900">
-    <div class="app-layout--bg relative flex grow bg-gradient-to-tl from-white to-accent-100 pt-12 dark:from-primary-700 dark:to-primary-900">
+    <div class="app-layout--bg relative flex grow bg-gradient-to-tl from-white to-accent-100 pt-6 dark:from-primary-700 dark:to-primary-900 md:pt-12">
       <div class="app-layout--bg-texture absolute inset-0 z-0 bg-[url('/assets/textures/nnnoise.svg')] opacity-10"></div>
       <div class="prose relative z-10 mx-auto flex flex-col px-6 dark:prose-invert md:prose-lg lg:prose-xl lg:min-w-[900px]">
         <nav-bar :pages="['index', 'work', 'contact']" />
--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,53 +1,32 @@
 <script setup lang="ts">
-import { HamburgerButton } from '@icon-park/vue-next'
-import { ref } from 'vue'
+import { useRoute } from 'vitepress'
+import { computed } from 'vue'
 import { $t, localePath } from '../utils/translate.utils'
 
 defineProps<{
   pages: string[]
 }>()
 
-const opened = ref(false)
-
-function toggle () {
-  opened.value = !opened.value
-}
+const actual = computed(() => {
+  const page = useRoute().path.split('.')[0]?.slice(1)
+  return page === '' ? 'index' : page
+})
 </script>
 
 <template>
   <div>
     <!-- eslint-disable sonar/no-vue-bypass-sanitization -->
-    <nav class="mb-24">
+    <nav class="mb-12 md:mb-24">
       <div class="mx-auto">
-        <div class="flex h-16 items-center justify-between">
-          <div class="flex w-full items-center justify-between">
-            <a class="app-link relative ml-8 flex shrink-0 flex-row items-center md:ml-0" href="/">
-              <span class="absolute -left-9 text-3xl">▷&nbsp;</span>Romain.cloud
-            </a>
-            <div class="hidden md:block">
-              <div class="ml-10 flex space-x-4">
-                <a v-for="page, index in pages" :key="`entry-${index}`" class="app-link" :href="localePath(page)">
-                  {{ $t(`pages.${page}`) }}
-                </a>
-                <dark-switch />
-              </div>
-            </div>
-          </div>
-          <div class="block">
-            <div class="ml-4 flex items-center md:ml-6"></div>
-          </div>
-          <div class="-mr-2 flex md:hidden">
-            <button type="button">
-              <HamburgerButton size="32" theme="outline" @click="toggle" />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="md:hidden">
-        <div class="flex flex-col gap-4 text-right sm:px-3">
-          <a v-for="page, index in pages" :key="`entry-${index}`" class="app-link" :href="localePath(page)">
+        <div class="flex w-full flex-wrap items-center justify-center gap-x-4 gap-y-2">
+          <a class="app-link relative -order-2 ml-8 flex shrink-0 flex-row items-center md:ml-0 md:mr-auto" href="/">
+            <span class="absolute -left-9 text-3xl">▷&nbsp;</span>Romain.cloud
+          </a>
+          <div class="w-full md:hidden"></div>
+          <a v-for="page, index in pages" :key="`entry-${index}`" class="app-link" :class="page === actual ? 'active' : ''" :href="localePath(page)">
             {{ $t(`pages.${page}`) }}
           </a>
+          <dark-switch class="-order-1 ml-auto md:order-1 md:ml-0" />
         </div>
       </div>
     </nav>
