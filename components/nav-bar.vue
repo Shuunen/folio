770514e0375d45e69bd993acad939ea405cf1c91
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-10-16 12:01:36 +0200

    enhance navbar responsiveness

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import { useRoute } from 'vitepress'
 import { computed } from 'vue'
+import { getPage } from '../utils/browser.utils'
 import { $t, localePath } from '../utils/translate.utils'
 
 defineProps<{
   pages: string[]
 }>()
 
-const actual = computed(() => {
-  const page = useRoute().path.split('.')[0]?.slice(1)
-  return page === '' ? 'index' : page
-})
+const actual = computed(() => getPage())
 </script>
 
 <template>
@@ -19,17 +16,26 @@ const actual = computed(() => {
     <nav class="mb-12 md:mb-24">
       <div class="mx-auto">
         <div class="flex w-full flex-wrap items-center justify-center gap-x-4 gap-y-2">
-          <a class="app-link relative -order-2 ml-8 flex shrink-0 flex-row items-center md:ml-0 md:mr-auto" href="/">
-            <span class="absolute -left-9 text-3xl">▷&nbsp;</span>Romain.cloud
+          <a class="app-link relative ml-10 mr-auto flex shrink-0 flex-row items-center md:ml-0" href="/">
+            <span class="absolute -left-9 text-3xl">▷&nbsp;</span>
+            <span class="hidden sm:block">Romain.cloud</span>
           </a>
-          <div class="w-full md:hidden"></div>
-          <a v-for="page, index in pages" :key="`entry-${index}`" class="app-link" :class="page === actual ? 'active' : ''" :href="localePath(page)">
+          <a v-for="page, index in pages" :key="`entry-${index}`" class="app-link"
+            :class="[page === actual ? 'app-link--active' : '', `app-link--${page}`]" :href="localePath(page)">
             {{ $t(`pages.${page}`) }}
           </a>
-          <dark-switch class="-order-1 ml-auto md:order-1 md:ml-0" />
+          <dark-switch />
         </div>
       </div>
     </nav>
   </div>
 </template>
+
+<style scoped>
+@media (max-width: 400px) {
+  .app-link--index {
+    @apply hidden;
+  }
+}
+</style>
 
