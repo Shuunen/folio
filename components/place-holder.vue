a993aef8e3526dca80400cbed83fba58f93574bc
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-06-18 21:48:59 +0200

    bump lock file & auto lint fixes

--- components/place-holder.vue
+++ components/place-holder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-24 w-60 rounded-md border-2">
     <div class="flex h-full animate-pulse flex-row items-center justify-center space-x-5">
-      <div class="h-12 w-12 rounded-full bg-gray-300"></div>
+      <div class="size-12 rounded-full bg-gray-300"></div>
       <div class="flex flex-col space-y-3">
         <div class="h-6 w-36 rounded-md bg-gray-300"></div>
         <div class="h-6 w-24 rounded-md bg-gray-300"></div>
