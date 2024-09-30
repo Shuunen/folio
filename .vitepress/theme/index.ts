a8ac1a8970e6f63898e3bc686d29bd4ab9402be8
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-10-01 01:01:38 +0200

    bump deps

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,7 +11,7 @@ const theme: Theme = {
     const components = import.meta.glob('../../components/*.vue', { eager: true }) // eslint-disable-line @typescript-eslint/naming-convention
     for (const [path, definition] of Object.entries(components)) {
       const componentName = path.split('/').pop()?.replace(/\.\w+$/u, '')
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/consistent-type-assertions
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/consistent-type-assertions
       if (componentName !== undefined) app.component(componentName, (definition as any).default)
     }
   },
