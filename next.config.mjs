4cec02e4abbc1dcbfe48369eccb03568db7d12d5
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-10-07 11:41:23 +0200

    add same next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   redirects() {
     return [
       // redirect url without language to default language
