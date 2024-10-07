7bfc5c17919756120c24451323ba3e0c675f6277
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-10-07 12:02:36 +0200

    disable redirects

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  redirects() {
-    return [
-      // redirect url without language to default language
-      {
-        source: '/',
-        destination: '/en',
-        permanent: true,
-      },
-      // redirect url with non-handled language to default language
-      {
-        source: '/:lang((?!en|fr).*)/:path*',
-        destination: '/en/:path*',
-        permanent: true,
-      },
-    ]
-  },
+  // redirects() {
+  //   return [
+  //     // redirect url without language to default language
+  //     {
+  //       source: '/',
+  //       destination: '/en',
+  //       permanent: true,
+  //     },
+  //     // redirect url with non-handled language to default language
+  //     {
+  //       source: '/:lang((?!en|fr).*)/:path*',
+  //       destination: '/en/:path*',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 }
 
 export default nextConfig
