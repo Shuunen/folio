8f6794ea446c4f61a2146c4ab7e9218a03aee1c1
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-11-07 14:23:18 +0100

    enable code coverage

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ['src/**/*.client.ts'],
+      include: ['src/utils'],
       reporter: ['text', 'lcov', 'html'],
+      thresholds: {
+        100: true,
+      },
     },
   },
 })
