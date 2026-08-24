67c02a498ce7f94e877116a5e7acf93b9c983b36
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-08-24 22:35:46 +0200

    chore: drop the vue website
    The folio stops being a hand maintained page. Remove the Vue, Vite &
    Tailwind site : its pages, components, layouts, locales, utils, assets
    and the tests that covered them, along with the tooling that only served
    it, eslint, postcss, tailwind, dependency-cruiser and the nonce script.
    The old typescript resume data goes too, the JSON Resume files replace it.

--- bin/inject-nonce.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./dist -type f -name "*.html" -print0 | xargs -0 sed -i 's/<script/<script nonce="shu7782n1"/g'
--- utils/storage.utils.ts
+++
@@ -1,6 +0,0 @@
-import { storage } from 'shuutils'
-
-storage.prefix = 'folio_'
-
-// eslint-disable-next-line unicorn/prefer-export-from
-export { storage }
